8ee91ea221094ccb1ce3d8d1a4ac0ecb6b3901c5
Shurpadrian <adrian.sanchez.escuderos@gmail.com>
2025-08-28 13:49:12 +0200

    Update config.py

--- tests/config.py
+++ tests/config.py
@@ -1,10 +1,13 @@
-# config.py - Gestor de configuración de tokens y IDs
-
 import os
 
-# Token y chat ID para Telegram, se leen de variables de entorno
+# Variables para entorno producción
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-if TELEGRAM_TOKEN is None or TELEGRAM_CHAT_ID is None:
-    raise ValueError("Debes definir las variables de entorno TELEGRAM_TOKEN y TELEGRAM_CHAT_ID")
+# Variables para entorno test
+TELEGRAM_TOKEN_TEST = os.getenv('TELEGRAM_TOKEN_TEST')
+TELEGRAM_CHAT_ID_TEST = os.getenv('TELEGRAM_CHAT_ID_TEST')
+
+# Puedes añadir validaciones simples para que existan al menos los de test
+if TELEGRAM_TOKEN_TEST is None or TELEGRAM_CHAT_ID_TEST is None:
+    raise ValueError("Debes definir las variables de entorno TELEGRAM_TOKEN_TEST y TELEGRAM_CHAT_ID_TEST")
